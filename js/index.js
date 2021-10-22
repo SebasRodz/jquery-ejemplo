fef24bbc96543b2cb4a02bd78145e23ee2aee325
SebasRodz <sebasxiom@gmail.com>
2021-10-21 20:24:22 -0500

    Agregando mas cosas

--- js/index.js
+++ js/index.js
@@ -2,4 +2,11 @@ $(document).ready(function(){
     $(".logo-h1").click(function(){
         $(".principal").toggle(300);
     })
+
+    $(".home").ready(function(){
+        $(".home-copy").addClass(["home"]);
+        var clase2 = $(".home").find("div").first();
+        $(clase2).addClass(["home-nav"]);
+        $(clase2).clone().appendTo(".home-copy");
+    })
 })
